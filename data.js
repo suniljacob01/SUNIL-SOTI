e2c2ffb630caba224ef63e0baf0a8218d7cdd74a
sunil-ship-it <suniljacob01@gmail.com>
2022-10-27 09:50:17 -0400

    initial commit
    initial commit

---
+++ data.js
@@ -0,0 +1,816 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-sot-hq-cafeteria",
+      "name": "SOT HQ Cafeteria",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.11199436135959573,
+        "pitch": 0.24553636841266346,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.5905428228110647,
+          "pitch": 0.13182128297487772,
+          "rotation": 0,
+          "target": "8-soti-hq-hallway-33"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-soti-hq-basket-ball-court",
+      "name": "SOTI HQ Basket Ball court",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.2096314941083772,
+        "pitch": -0.41129338107902313,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.560809016462314,
+          "pitch": 0.0023381600310194983,
+          "rotation": 0,
+          "target": "12-soti-hq-hallway2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-soti-hq-cafeteria2",
+      "name": "SOTI HQ Cafeteria2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.21632026875488286,
+        "pitch": 0.24193409348027117,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5617804377074265,
+          "pitch": -0.17405530478199083,
+          "rotation": 0,
+          "target": "6-soti-hq-gym"
+        },
+        {
+          "yaw": 0.16768372374679785,
+          "pitch": 0.1271297508962448,
+          "rotation": 0,
+          "target": "0-sot-hq-cafeteria"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-soti-hq-corporate-meeting-room",
+      "name": "SOTI HQ Corporate meeting Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.3320769073359617,
+        "pitch": -0.06958667567369226,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.4981986763394906,
+          "pitch": 0.15674826136415376,
+          "rotation": 0,
+          "target": "10-soti-hq-hallway11"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-soti-hq-entrance1",
+      "name": "SOTI HQ Entrance1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.6138781101240003,
+        "pitch": -0.06885816506746067,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6153460646247808,
+          "pitch": -0.06739021056668015,
+          "rotation": 6.283185307179586,
+          "target": "18-soti-hq-reception"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-soti-hq-gaming-room",
+      "name": "SOTI HQ Gaming Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.3061038995805383,
+        "pitch": -0.12468910971675129,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.1035113889776778,
+          "pitch": -0.020437335133234313,
+          "rotation": 0,
+          "target": "8-soti-hq-hallway-33"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.990020113310834,
+          "pitch": -0.3872416171468753,
+          "title": "SOTI-HQ-Exit",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "6-soti-hq-gym",
+      "name": "SOTI HQ Gym",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.631003652644111,
+        "pitch": -0.20285256604234192,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.12750682862408,
+          "pitch": -0.08343485273958429,
+          "rotation": 0,
+          "target": "0-sot-hq-cafeteria"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-soti-hq-hallway-1",
+      "name": "SOTI HQ Hallway 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.07640113033230733,
+        "pitch": -0.037220629766194335,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.6587181657535623,
+          "pitch": 0.17166655151123322,
+          "rotation": 0,
+          "target": "2-soti-hq-cafeteria2"
+        },
+        {
+          "yaw": -0.36621200738200166,
+          "pitch": -0.06775297497195076,
+          "rotation": 0,
+          "target": "12-soti-hq-hallway2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.6986402241879759,
+          "pitch": -0.08723076916227157,
+          "title": "SOTI Aerospace",
+          "text": "Innovation Area"
+        },
+        {
+          "yaw": -0.05677689526993035,
+          "pitch": 0.04402603661839777,
+          "title": "IT",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "8-soti-hq-hallway-33",
+      "name": "SOTI HQ Hallway 3.3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.8800363516387257,
+        "pitch": -0.06885796627568297,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5015193945911349,
+          "pitch": -0.1809333362600416,
+          "rotation": 0,
+          "target": "14-soti-hq-hallway32"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-soti-hq-hallway1-leftside",
+      "name": "SOTI HQ Hallway1 Leftside",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.1358065747593944,
+        "pitch": -0.08560744846225177,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.7699856169233037,
+          "pitch": -0.05153213876431728,
+          "rotation": 0,
+          "target": "3-soti-hq-corporate-meeting-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-soti-hq-hallway11",
+      "name": "SOTI HQ Hallway1.1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.067144968132121,
+        "pitch": 0.23243562411866137,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8734612308938194,
+          "pitch": 0.14974872151162444,
+          "rotation": 0,
+          "target": "7-soti-hq-hallway-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-soti-hq-hallway12",
+      "name": "SOTI HQ Hallway1.2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.8997237627477208,
+        "pitch": -0.17493695990111036,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.014840371498229,
+          "pitch": 0.11358520376021275,
+          "rotation": 0,
+          "target": "9-soti-hq-hallway1-leftside"
+        },
+        {
+          "yaw": -0.2763150581874214,
+          "pitch": -0.09633703733312515,
+          "rotation": 0,
+          "target": "16-soti-hq-larger-meeting-room"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.09115215212747785,
+          "pitch": 0.21383960307705152,
+          "title": "Sitting Area",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.022603458316393,
+          "pitch": -0.13130279744378015,
+          "title": "Cafeteria Entrance",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.1527225811977182,
+          "pitch": -0.09113860802957419,
+          "title": "Large Meeting Room",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "12-soti-hq-hallway2",
+      "name": "SOTI HQ Hallway2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.264692427214369,
+        "pitch": 0.09677104670319991,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.7994560351917794,
+          "pitch": -0.10141833943232292,
+          "rotation": 0,
+          "target": "1-soti-hq-basket-ball-court"
+        },
+        {
+          "yaw": -1.2741306835861685,
+          "pitch": -0.20077326110097538,
+          "rotation": 0,
+          "target": "2-soti-hq-cafeteria2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.9420016154022726,
+          "pitch": -0.07899725758909248,
+          "title": "Direction to Basket ball Room",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "13-soti-hq-hallway31",
+      "name": "SOTI HQ Hallway3.1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.4862149861213716,
+        "pitch": 0.29776503812956,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1920831465971702,
+          "pitch": 0.26175163121520306,
+          "rotation": 1.5707963267948966,
+          "target": "5-soti-hq-gaming-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "14-soti-hq-hallway32",
+      "name": "SOTI HQ Hallway3.2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.9290521743462357,
+        "pitch": 0.029776491712969033,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1402266868863817,
+          "pitch": 0.0036078849753433673,
+          "rotation": 0,
+          "target": "17-soti-hq-prometus-room"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.9785482887718686,
+          "pitch": 0.13011602919135434,
+          "title": "Senior leadership team",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "15-soti-hq-hallway3",
+      "name": "SOTI HQ Hallway3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.3620205955787199,
+        "pitch": 0.2314516196139138,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.25839822605883356,
+          "pitch": 0.03744865736327441,
+          "rotation": 0,
+          "target": "13-soti-hq-hallway31"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "16-soti-hq-larger-meeting-room",
+      "name": "SOTI HQ Larger Meeting Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.08055365778435508,
+        "pitch": 0.2754273708836852,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "17-soti-hq-prometus-room",
+      "name": "SOTI HQ Prometus Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.159764754605634,
+        "pitch": 0.1023567318570322,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.759861728579436,
+          "pitch": 0.07415068714966999,
+          "rotation": 0,
+          "target": "15-soti-hq-hallway3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "18-soti-hq-reception",
+      "name": "SOTI HQ Reception",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.288409584379659,
+        "pitch": 0.20842377251683253,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0931442187060636,
+          "pitch": -0.11860654642443436,
+          "rotation": 6.283185307179586,
+          "target": "11-soti-hq-hallway12"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.8571208032632605,
+          "pitch": -0.021614726495959857,
+          "title": "Reception Desk",
+          "text": "Please enquire abou the person whom you would like to contact. Deliver the courier at receptioon"
+        },
+        {
+          "yaw": -2.442321689041613,
+          "pitch": -0.14426890397493963,
+          "title": "Sitting Area",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
